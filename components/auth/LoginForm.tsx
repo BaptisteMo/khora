88ba257484a14fec3cb38afb1b1a9bcdeb1544af
BaptisteMo <morillon.bapt@gmail.com>
2025-04-17 14:19:43 +0200

    fix: typo fix

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -92,7 +92,7 @@ export default function LoginForm() {
       
       <div className="mt-4 text-center">
         <p className="text-sm text-gray-600">
-          Don't have an account?{' '}
+          Dont have an account?{' '}
           <Link href="/register" className="text-primary hover:underline">
             Register
           </Link>
